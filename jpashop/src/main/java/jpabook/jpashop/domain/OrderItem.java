8192067497705ea0bbe5dbbf73078135e55256b2
techhan <it_hs@naver.com>
2022-06-27 23:25:52 +0900

    스프링 부트와 JPA 활용1 - 주문, 주문상품 엔티티 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -26,4 +27,27 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; // 주문 수량
+
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    //==비즈니스 로직==//
+    public void cancel() { // 재고 수량을 원복해주는 메서드
+        getItem().addStock(count);
+
+    }
+    
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
