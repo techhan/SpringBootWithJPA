2ed145fbea3994e6d92a2169b4196846a9cc7dff
techhan <it_hs@naver.com>
2022-06-28 16:46:26 +0900

    스프링 부트와 JPA 활용1 - 변경 감지와 병합(merge)

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -63,8 +63,23 @@ public class ItemController {
     }
 
     @PostMapping("/items/{itemId}/edit")
-    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form) {
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+        /** 준영속 엔티티
+         * 영속성 컨텍스트가 더는 관리하지 않는 엔티티.
+         * 여기에서는 itemService.saveItem(book)에서 수정을 시도하는 Book 객체이다.
+         * Book 객체는 이미 DB에 저장이 한 번 되어서 식별자가 존재한다.
+         * 이렇게 임의로 만들어낸 엔티티도 기존 식별자를 가지고 있으면 준영속 엔티티로 보는데..
+         * 준영속 엔티티는 JPA에서 관리를 안하므로 변경이 일어날 시 update 처리를 따로 해주지 않는다.
+         *
+         * 준영속 엔티티를 수정하는 두가지 방법
+         * 1. 변경 감지 기능 사용
+         * 2. 병합(merge) 사용
+         *
+         * 변경 감지 기능을 사용하면 원하는 속성만 선택해서 변경할 수 있지만, 병합을 사용하면 모든 속성이 변경된다.
+         * 병합시 값이 없으면 null로 업데이트할 위험성이 존재한다...! 그러므로 되도록 변경 감지 기능을 사용해야 맞다.
+         */
 
+        /*
         Book book = new Book();
         book.setId(form.getId());
         book.setName(form.getName());
@@ -72,8 +87,9 @@ public class ItemController {
         book.setStockQuantity(form.getStockQuantity());
         book.setAuthor(form.getAuthor());
         book.setIsbn(form.getIsbn());
+        */
 
-        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 
