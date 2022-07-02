d0639f3a258a235c579bf244a483f8ceef523d54
techhan <it_hs@naver.com>
2022-07-03 03:14:24 +0900

    자바 ORM 표준 JPA 프로그래밍 - 다양한 연관관계 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,6 +3,7 @@ package hellojpa;
 import com.sun.istack.internal.NotNull;
 
 import javax.persistence.*;
+import java.util.concurrent.locks.Lock;
 
 @Entity // 이 애노테이션이 있어야 JPA가 관리할 객체임을 인식한다.
 //@SequenceGenerator(
@@ -24,6 +25,10 @@ public class Member {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     public Long getId() {
         return id;
