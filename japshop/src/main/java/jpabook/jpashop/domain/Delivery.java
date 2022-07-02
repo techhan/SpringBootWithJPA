6564ab2963ea5bb062926edfeed63a4b6e7f927e
techhan <it_hs@naver.com>
2022-07-03 03:28:11 +0900

    자바 ORM 표준 JPA 프로그래밍 - 실전 예제3-다양한 연관관계 매핑

---
+++ japshop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+
+
+}
