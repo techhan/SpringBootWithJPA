765ba18ab90e9f0028b819675d439639b0224a24
techhan <it_hs@naver.com>
2022-07-17 22:14:27 +0900

    프로젝션, 페이징, 조인

---
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,53 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 엔티티 매니저 생성
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            for(int i = 0; i < 100; i++) {
+
+                Team team = new Team();
+                team.setName("teamA");
+                em.persist(team);
+
+                Member member = new Member();
+                member.setUsername("teamA");
+                member.setAge(10);
+                member.setTeam(team);
+                em.persist(member);
+            }
+
+            em.flush();
+            em.clear();
+
+            String query = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            System.out.println("result.size() = " + result.size());
+
+
+//            Member findMember = result.get(0);
+//            findMember.setAge(20);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            // 엔티티 매니저 종료
+            em.close();
+        }
+        // 엔티티 매니저 팩토리 종료
+        emf.close();
+    }
+}
